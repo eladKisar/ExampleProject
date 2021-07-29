cb32a640501ddb5f1916affde84e9643f506035b
user <user@DESKTOP-VMQINFL>
2021-07-29 14:33:52 +0300

    make it better

--- createConnection.ts
+++ createConnection.ts
@@ -10,6 +10,7 @@ export let dbOptions: ConnectionOptions = {
     username:process.env.DB_USERNAME,
     password:process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    options: {encrypt: true},
     "synchronize": false,
     "logging": true,
     "entities": ["src/entity/**/*.ts"],
